75026d01793f62f2c8470f07ce9d1cf0eb698f6f
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-21 17:10:53 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name
   def initialize(attributes)
     attributes.each do |key,val|
-      self.send("@#{key}= #{val}")
+      self.send("#{key}= #{val}")
     end
   end
 end
