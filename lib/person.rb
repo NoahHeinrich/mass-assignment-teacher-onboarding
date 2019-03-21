573fc9c390b90f7f5eb527e97fda5a196208bf48
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-21 17:13:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size
   def initialize(attributes)
     attributes.each do |key,val|
       self.send(("#{key}="), val)
